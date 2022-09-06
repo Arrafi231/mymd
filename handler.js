88f846a00716aa3248c1a286c883b4cc36e0538d
Arrafi231 <91902042+Arrafi231@users.noreply.github.com>
2022-09-06 18:37:39 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -30,19 +30,19 @@ module.exports = {
         global.doc = pickRandom(["application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.presentationml.presentation", "application/msword", "application/pdf"])
         global.img = 'https://telegra.ph/file/0c567eac403737766688f.jpg'
         global.fla = pickRandom(global.flaaa)
-        global.namabot = conn.user.name
-        global.packname = '© CUTE IQ-MD'
-        global.author = 'Made By Ziv San © 2022'
+        global.namabot = Rafii-Botzz
+        global.packname = '© 𝕽𝖆𝖋𝖎𝖎-𝕭𝖔𝖙𝖟𝖟'
+        global.author = 'Ｍａｄｅ Ｂｙ Ｒａｆｉｉ© 2022'
         global.packname2 = 'Made With'
-        global.author2 = 'Cute Bot By Ziv San © 2022'
-        global.wm3 = '·͟͟͟͟͟͟͞͞͞͞͞͞Made By Ziv San˚'
-        global.wm2 = '© CUTE IQ-MD' + ' ' + 'By Ziv San'
-        global.wm = '© CUTE IQ-MD'
+        global.author2 = '𝚃𝚈𝚄𝚄 𝙱𝙾𝚃𝚉𝚉© 2022'
+        global.wm3 = '·͟͟͟͟͟͟͞͞͞͞͞͞Rafii-Botzz˚'
+        global.wm2 = '© ·͟͟͟͟͟͟͞͞͞͞͞͞Rafii-Botzz˚' + ' ' + 'By Ziv San'
+        global.wm = '© ·͟͟͟͟͟͟͞͞͞͞͞͞Rafii-Botzz˚'
         global.colong1 = 'Ciss 📸'
-        global.colong2 = 'By Haori & Ziv San'
+        global.colong2 = 'By Rafii & Tyuu'
         global.kontak2 = [
-        ['16199961931', 'Ziv San', 'Creator Bot', 'Zivsan@support.com', true],
-        ['6282179137771', 'CUTE IQ-MD', 'Bot WhatsApp', 'Just Bot', true]
+        ['6285774269597', 'Rafii', 'Creator Bot', 'Zivsan@support.com', true],
+        ['6285953825513', 'tyuuu', 'Bot WhatsApp', 'Just Bot', true]
         ]
         global.bg = await (await fetch(img)).buffer()
         global.time = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss')
